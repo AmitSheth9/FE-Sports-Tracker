705e045cbfa225eb427edfe4c1e37e71b08a8140
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-06 05:46:58 -0600

    changed intro mess

--- src/Views/BetForm/BetInputForm.jsx
+++ src/Views/BetForm/BetInputForm.jsx
@@ -139,7 +139,7 @@ return (
     {auth.username ? 
     <p>You are logged in as {auth.username}</p> : 
     <p>You are not logged in</p>}
-    <p>Welcome to Bettracker. The purpose of this site is to give sports bettors a place to track their bets and obtain useful data about their betting patterns.  Submit your bet details below and then view analysis <Link to='/betdata'>here</Link> </p>
+    <p>Welcome to Bettracker. The purpose of this site is to give sports fans a place to track their bets and obtain useful data about their betting patterns.  Submit your bet details below and then view analysis <Link to='/betdata'>here</Link> </p>
     <p className = 'betmessage'>Enter your bet details below. All fields are optional. The more data the better the analysis</p>
 <div >
     <form className="form-container" onSubmit={handleSubmit}>
