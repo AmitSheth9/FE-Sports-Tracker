539b078edd567780679d54a6ab679960fbb97cf4
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-10 02:07:31 -0600

    changed font and layout

--- src/Views/BetAnalysis/BetData.jsx
+++ src/Views/BetAnalysis/BetData.jsx
@@ -105,9 +105,9 @@ export default function BetData() {
             <div className='login-status'>Logged in as {auth.username}</div> : 
             <div className='login-status'>Not logged in</div>}
             {!betData.length && 
-            <div className = 'nobets'>You have not submitted any bets to track. Submit a bet in the <Link to='/'>BetForm</Link></div>}
+            <div className = 'nobets'>You have not submitted any bets to track. Submit a bet in the <Link to='/'>BetForm</Link></div>}<br/><br/>
             <div className='stats-container'>
-            <div className = 'summary'>
+            <div className = 'summary'>Summary
                 <div className='heading'>Bets Tracked: {betData.length}</div>
                 <div className='heading'>Record: {wins}-{losses}-{pushes}</div>
                 <div className='heading'>Net result: ${Number(netResult.toFixed(2))}</div>
